64ae6b7a816c8e1cb33019065a0e22a039d7cbe4
tatayna-razgulyaeva <tatanarazgulaeva16@gmail.com>
2024-04-07 22:35:08 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        var firstName = "Ivan";
+        var middleName = "Ivanov";
+        var lastName = "Ivanovich";
+        var fullName = middleName + " " + firstName + " " + lastName;
+        System.out.println(fullName);
+
+        System.out.println(fullName.toUpperCase());
+
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println(fullName);
     }
 }
