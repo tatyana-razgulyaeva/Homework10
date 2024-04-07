dc71a7cbbe213087050666bf43b286161e6d2844
tatayna-razgulyaeva <tatanarazgulaeva16@gmail.com>
2024-04-07 23:47:26 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
-import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String[] args) {
 
-        var firstName = "Ivan";
-        var middleName = "Ivanov";
-        var lastName = "Ivanovich";
-        var fullName = middleName + " " + firstName + " " + lastName;
-        System.out.println(fullName);
+        String firstName = "Ivan";
+        String middleName = "Ivanov";
+        String lastName = "Ivanovich";
+        String fullName = middleName + " " + firstName + " " + lastName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
 
-        System.out.println(fullName.toUpperCase());
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
 
         fullName = "Иванов Семён Семёнович";
         fullName = fullName.replace("ё", "е");
-        System.out.println(fullName);
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
     }
 }
